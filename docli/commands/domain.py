e1a898af618195d05f8f3c0456805cd8ac751316
Yogesh Panchal <yspanchal@gmail.com>
2015-06-01 22:39:00 +0530

    added domain create and list method

--- docli/commands/domain.py
+++ docli/commands/domain.py
@@ -13,49 +13,70 @@ def domain_group():
 	pass
 
 
-def validate_list(ctx, param, value):
-	if value:
-		if ctx.params.has_key('create'):
-			if ctx.params['create']:
-				raise click.UsageError('Invalid option combination %s & %s' %(param.name, 'create'))
-	return value
+def validate(dic):
+	if dic['create'] and dic['getlist']:
+		raise click.UsageError('Invalid option combination --create cannot be used with --getlist')
 
+	if dic['getlist'] and dic['name']:
+		raise click.UsageError('Invalid option combination --getlist cannot be used with --name')
 
-def validate_create(ctx, param, value):
-	# print ctx.params
-	if value:
-		if ctx.params.has_key('getlist'):
-			if ctx.params['getlist']:
-				raise click.UsageError('Invalid option combination %s & %s' %(param.name, 'create'))
-	return value
+	if dic['getlist'] and dic['ip']:
+		raise click.UsageError('Invalid option combination --getlist cannot be used with --ip')
+
+	if dic['create'] and dic['ip'] and not dic['name']:
+		raise click.UsageError('--name or -n domain name missing')
+
+	if dic['create'] and dic['name'] and not dic['ip']:
+		raise click.UsageError('--ip or -i domain ip missing')
+
+	if dic['name'] and not dic['create']:
+		raise click.UsageError('--create or -c domain create option missing')
+
+	if dic['ip'] and not dic['create']:
+		raise click.UsageError('--create or -c domain create option missing')
+
+	return True
 
 
 @domain_group.command(context_settings=CONTEXT_SETTINGS)
-@click.option('--create', '-c', help='create new sdomain', is_eager=True, callback=validate_create)
-@click.option('--getlist', '-l', is_flag=True, help='list all actions', is_eager=True, callback=validate_list)
+@click.option('--getlist', '-l', is_flag=True, help='list all actions', is_eager=True)
+@click.option('--create', '-c', is_flag=True, help='create new domain', is_eager=True)
+@click.option('--name', '-n', type=str, help='domain name entry', metavar='<example.com>')
+@click.option('--ip', '-i', type=str, help='ip address for domain', metavar='<1.2.3.4>')
 @click.option('--token', '-t', type=str, help='digital ocean authentication token', metavar='<token>')
 @click.option('--tablefmt', '-f', type=click.Choice(['fancy_grid', 'simple', 'plain', 'grid', 'pipe', 'orgtbl', 'psql', 'rst', 'mediawiki', 'html', 'latex', 'latex_booktabs', 'tsv']), help='output table format', default='fancy_grid', metavar='<format>')
 @click.option('--proxy', '-p', help='proxy url to be used for this call', metavar='<http://ip:port>')
 @click.pass_context
-def domain(ctx, token, tablefmt, proxy, getlist, create):
+def domain(ctx, token, tablefmt, proxy, getlist, create, name, ip):
 	"""
 	Domains that you are managing through the DigitalOcean DNS interface.
 	"""
-	if getlist:
-		method = 'GET'
-		url = DOMAIN_LIST
-		result = DigitalOcean.do_request(method, url, token=token, proxy=proxy)
-		if result['has_error']:
-			click.echo()
-			click.echo(result['error_message'])
-		else:
-			record = 'domain'
-			headers = ['Domain Name']
-			table = []
-			for domain in result['domains']:
-				table.append([domain['name']])
-			data = {'headers': headers, 'table_data': table}
-			print_table(tablefmt, data, record)
+	if validate(ctx.params):
+		if getlist:
+			method = 'GET'
+			url = DOMAIN_LIST
+			result = DigitalOcean.do_request(method, url, token=token, proxy=proxy)
+			if result['has_error']:
+				click.echo()
+				click.echo(result['error_message'])
+			else:
+				record = 'domain'
+				headers = ['Domain Name']
+				table = []
+				for domain in result['domains']:
+					table.append([domain['name']])
+				data = {'headers': headers, 'table_data': table}
+				print_table(tablefmt, data, record)
 
-	if create:
-		print "create"
+		if create:
+			method = 'POST'
+			url = DOMAIN_LIST
+			params = {'name': name, 'ip_address': ip}
+			result = DigitalOcean.do_request(method, url, token=token, proxy=proxy, params=params)
+			if result['has_error']:
+				click.echo()
+				click.echo(result['error_message'])
+			else:
+				click.echo()
+				click.echo("Domain Created ", name)
+				click.echo()
