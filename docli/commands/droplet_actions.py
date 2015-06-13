330e2fabe67ec3d27d8053250eaf2d49c1637f54
Yogesh Panchal <yspanchal@gmail.com>
2015-06-13 21:20:04 +0530

    added password reset droplet action command

--- docli/commands/droplet_actions.py
+++ docli/commands/droplet_actions.py
@@ -24,14 +24,16 @@ def validate(dic):
 @click.option('--shutdown', '-s', type=int, help='Shutdown droplet for given droplet id', metavar='<3812352>')
 @click.option('--power-off', '-P', type=int, help='Power Off droplet for given droplet id', metavar='<3812352>')
 @click.option('--power-on', '-o', type=int, help='Power On droplet for given droplet id', metavar='<3812352>')
+@click.option('--power-on', '-o', type=int, help='Power On droplet for given droplet id', metavar='<3812352>')
+@click.option('--password-reset', '-w', type=int, help='Password Reset droplet for given droplet id', metavar='<3812352>')
 @click.pass_context
-def droplet_actions(ctx, disable_backups, reboot, shutdown, power_off, power_on):
+def droplet_actions(ctx, disable_backups, reboot, shutdown, power_off, power_on, password_reset):
 	"""
 	Droplet actions are tasks that can be executed on a Droplet.
 	These can be things like rebooting, resizing, snapshotting, etc.
 	"""
 
-	if (not ctx.params['disable_backups'] and not ctx.params['reboot'] and not ctx.params['power_cycle'] and not ctx.params['shutdown'] and not ctx.params['power_off'] and not ctx.params['power_on']):
+	if (not ctx.params['disable_backups'] and not ctx.params['reboot'] and not ctx.params['power_cycle'] and not ctx.params['shutdown'] and not ctx.params['power_off'] and not ctx.params['power_on'] and not ctx.params['password_reset']):
 		return click.echo(ctx.get_help())
 
 	if validate(ctx.params):
@@ -142,3 +144,21 @@ def droplet_actions(ctx, disable_backups, reboot, shutdown, power_off, power_on)
 				click.echo()
 				click.echo('To get status update of above action execute following command.')
 				click.echo('Command: docli action -i %d' % power_on)
+
+		if password_reset:
+			method = 'POST'
+			url = DROPLETS + str(password_reset) + '/actions'
+			params = {'type':'password_reset'}
+			result = DigitalOcean.do_request(method, url, token=token, proxy=proxy, params=params)
+			if result['has_error']:
+				click.echo()
+				click.echo('Error: %s' %(result['error_message']))
+			else:
+				record = 'droplet password_reset'
+				headers = ['Fields', 'Values']
+				table = [['Id', result['action']['id']], ['Status', result['action']['status']], ['Type', result['action']['type']], ['Started at', result['action']['started_at']], ['Completed at', result['action']['completed_at']], ['Resource Id', result['action']['resource_id']], ['Resource Type', result['action']['resource_type']], ['Region', result['action']['region']]]
+				data = {'headers': headers, 'table_data': table}
+				print_table(tablefmt, data, record)
+				click.echo()
+				click.echo('To get status update of above action execute following command.')
+				click.echo('Command: docli action -i %d' % password_reset)
