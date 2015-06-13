67ea5f2aa962bce785278870633b44e8928723b3
Yogesh Panchal <yspanchal@gmail.com>
2015-06-13 21:07:35 +0530

    added power off droplet action command

--- docli/commands/droplet_actions.py
+++ docli/commands/droplet_actions.py
@@ -22,14 +22,15 @@ def validate(dic):
 @click.option('--reboot', '-r', type=int, help='Reboot droplet for given droplet id', metavar='<3812352>')
 @click.option('--power-cycle', '-p', type=int, help='Power cycle droplet for given droplet id', metavar='<3812352>')
 @click.option('--shutdown', '-s', type=int, help='Shutdown droplet for given droplet id', metavar='<3812352>')
+@click.option('--power-off', '-s', type=int, help='Power Off droplet for given droplet id', metavar='<3812352>')
 @click.pass_context
-def droplet_actions(ctx, disable_backups, reboot, shutdown):
+def droplet_actions(ctx, disable_backups, reboot, shutdown, power_off):
 	"""
 	Droplet actions are tasks that can be executed on a Droplet.
 	These can be things like rebooting, resizing, snapshotting, etc.
 	"""
 
-	if (not ctx.params['disable_backups'] and not ctx.params['reboot'] and not ctx.params['power_cycle'] and not ctx.params['shutdown']):
+	if (not ctx.params['disable_backups'] and not ctx.params['reboot'] and not ctx.params['power_cycle'] and not ctx.params['shutdown'] and not ctx.params['power_off']):
 		return click.echo(ctx.get_help())
 
 	if validate(ctx.params):
@@ -103,4 +104,22 @@ def droplet_actions(ctx, disable_backups, reboot, shutdown):
 				print_table(tablefmt, data, record)
 				click.echo()
 				click.echo('To get status update of above action execute following command.')
-				click.echo('Command: docli action -i %d' % shutdown
+				click.echo('Command: docli action -i %d' % shutdown)
+
+		if power_off:
+			method = 'POST'
+			url = DROPLETS + str(power_off) + '/actions'
+			params = {'type':'power_off'}
+			result = DigitalOcean.do_request(method, url, token=token, proxy=proxy, params=params)
+			if result['has_error']:
+				click.echo()
+				click.echo('Error: %s' %(result['error_message']))
+			else:
+				record = 'droplet power_off'
+				headers = ['Fields', 'Values']
+				table = [['Id', result['action']['id']], ['Status', result['action']['status']], ['Type', result['action']['type']], ['Started at', result['action']['started_at']], ['Completed at', result['action']['completed_at']], ['Resource Id', result['action']['resource_id']], ['Resource Type', result['action']['resource_type']], ['Region', result['action']['region']]]
+				data = {'headers': headers, 'table_data': table}
+				print_table(tablefmt, data, record)
+				click.echo()
+				click.echo('To get status update of above action execute following command.')
+				click.echo('Command: docli action -i %d' % power_off)
