4f93c31519f8c72bd9b8b4772c20216bd751e9ea
Yogesh Panchal <yspanchal@gmail.com>
2015-06-16 22:25:55 +0530

    added add ssh key command

--- docli/commands/ssh_keys.py
+++ docli/commands/ssh_keys.py
@@ -26,16 +26,20 @@ def invoke_list(token, proxy, page=1):
 
 @ssh_keys_group.command(name='ssh-key', context_settings=CONTEXT_SETTINGS)
 @click.option('--getlist', '-l', is_flag=True, help='list all ssh keys')
+@click.option('--create', '-c', is_flag=True, help='add new ssh key')
+@click.option('--name', '-n', type=str, help='name of ssh key', metavar='<My SSH Key>')
+@click.option('--key', '-k', type=str, help='public ssh keys string', metavar='<ssh-rsa xxx... >')
 @click.option('--token', '-t', type=str, help='digital ocean authentication token', metavar='<token>')
 @click.option('--tablefmt', '-f', type=click.Choice(['fancy_grid', 'simple', 'plain', 'grid', 'pipe', 'orgtbl', 'psql', 'rst', 'mediawiki', 'html', 'latex', 'latex_booktabs', 'tsv']), help='output table format', default='fancy_grid', metavar='<format>')
 @click.option('--proxy', '-p', help='proxy url to be used for this call', metavar='<http://ip:port>')
 @click.pass_context
-def ssh_keys(ctx, getlist, token, tablefmt, proxy):
+def ssh_keys(ctx, getlist, create, name, key, token, tablefmt, proxy):
 	"""
 	DigitalOcean allows you to add SSH public keys to the interface 
 	so that you can embed your public key into a Droplet at the time of creation.
 	"""
-	if (not ctx.params['getlist']):
+	if (not ctx.params['getlist'] and not ctx.params['create'] and not ctx.params['name'] 
+		and not ctx.params['key']):
 		return click.echo(ctx.get_help())
 
 	if validate(ctx.params):
@@ -51,7 +55,7 @@ def ssh_keys(ctx, getlist, token, tablefmt, proxy):
 					click.echo('Error: %s' %(result['error_message']))
 				else:
 					headers = ['Fields', 'Values']
-					for dic in result['images']:
+					for dic in result['ssh_keys']:
 						table = [['Id', dic['id']], ['Finger Print', dic['fingerprint']], ['Public Key', dic['public_key']], 
 						['Name', dic['name']]]
 						data = {'headers': headers, 'table_data': table}
@@ -68,3 +72,17 @@ def ssh_keys(ctx, getlist, token, tablefmt, proxy):
 							has_page = False
 					else:
 						has_page = False
+
+		if create:
+			method = 'POST'
+			url = KEYS
+			params = {'name':name, 'public_key':key}
+			result = DigitalOcean.do_request(method, url, token=token, proxy=proxy, params=params)
+			if result['has_error']:
+				click.echo()
+				click.echo('Error: %s' %(result['error_message']))
+			else:
+				headers = ['Fields', 'Values']
+				table = [['Id', result['ssh_key']['id']], ['Finger Print', result['ssh_key']['fingerprint']], ['Public Key', result['ssh_key']['public_key']], ['Name', result['ssh_key']['name']]]
+				data = {'headers': headers, 'table_data': table}
+				print_table(tablefmt, data, record)
