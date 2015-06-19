94c59d3bceba5806fc894abe13e96a735280e1bb
Yogesh Panchal <yspanchal@gmail.com>
2015-06-20 00:23:13 +0530

    fixed configure command

--- docli/docli.py
+++ docli/docli.py
@@ -19,7 +19,7 @@ sources_list = [account_group, actions_group, domain_group, record_group,
 				droplet_group, droplet_actions_group, images_group, 
 				image_actions_group, ssh_keys_group, region_size_group]
 
-def config_file(file):
+def config_file(file, reconfigure=False):
 	"""
 	create configuration file
 	"""
@@ -27,6 +27,28 @@ def config_file(file):
 	f = open(file, 'w')
 	f.write('[docli]\nauth_token='+value)
 	f.close()
+	if not reconfigure:
+		bashrc = os.path.expanduser('~/.bashrc')
+		bash_profile = os.path.expanduser('~/.bash_profile')
+		profile = os.path.expanduser('~/.profile')
+		if os.path.isfile(bashrc):
+			bashrc_file = open(bashrc, 'a')
+			bashrc_file.write('eval "$(_DOCLI_COMPLETE=source docli)"')
+			bashrc_file.close()
+			click.echo("apply changes by running 'source ~/.bashrc' without ''")
+		elif os.path.isfile(bash_profile):
+			bash_profile_file = open(bash_profile, 'a')
+			bash_profile_file.write('eval "$(_DOCLI_COMPLETE=source docli)"')
+			bash_profile_file.close()
+			click.echo("apply changes by running 'source ~/.bash_profile' without ''")
+		elif os.path.isfile(profile):
+			profile_file = open(profile, 'a')
+			profile_file.write('eval "$(_DOCLI_COMPLETE=source docli)"')
+			profile_file.close()
+			click.echo("apply changes by running 'source ~/.profile' without ''")
+		else:
+			msg = 'Add following line to your bashrc.\n eval "$(_DOCLI_COMPLETE=source docli)"'
+			click.echo(msg)
 	click.echo()
 	click.echo("configuration completed.")
 
@@ -54,7 +76,8 @@ def docli(configure):
 		else:
 			value = click.prompt('Do you want to reconfigure docli [y/n] ?', type=str, default='n')
 			if value.lower() == 'y':
-				config_file(file)
+				reconfigure = True
+				config_file(file, reconfigure)
 			else:
 				click.echo()
 				click.echo('done..!!!')
